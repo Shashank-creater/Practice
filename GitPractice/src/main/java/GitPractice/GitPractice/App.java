649d6f4d931209806e7e4b0e0cb111e974b70643
SG03215815 <SG03215815@SIPL-133.softenger.local>
2022-01-23 12:47:49 +0530

    jaj

--- GitPractice/src/main/java/GitPractice/GitPractice/App.java
+++ GitPractice/src/main/java/GitPractice/GitPractice/App.java
@@ -10,7 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! ***8" );
+        System.out.println( "Hello World! My Java" );
         
         new Addition().add(4, 5);
     }
